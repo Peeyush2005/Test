fa9431a2125a67cf4ab9f136044de1a06778692f
“Peeyush <peeyushrampal31@gmail.com>
2025-06-05 22:27:57 +0530

    first commit

---
+++ sum.py
@@ -0,0 +1,7 @@
+def simple_sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    print("Sum:", simple_sum(x, y))
