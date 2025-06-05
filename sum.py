28af6bdcd09833b0e4c05ec23bbe2e8d629242e5
AkshayRaoA <aroorakshay@gmail.com>
2025-06-05 22:35:47 +0530

    2nd commit by akshay

--- sum.py
+++ sum.py
@@ -5,3 +5,8 @@ if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
     print("Sum:", simple_sum(x, y))
+
+
+    #this is first commit for test branch
+    #commits in sum.py
+
